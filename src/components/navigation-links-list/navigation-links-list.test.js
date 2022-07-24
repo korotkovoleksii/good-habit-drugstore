e8e862b832740d0be9d191e0fa71de6cc1b00b2d
alekseikorotkov <korotkovaleksei@icloud.com>
2022-07-24 18:23:50 +0300

    feat: add tests for NavigationLinksList and NavigationLink components

--- src/components/navigation-links-list/navigation-links-list.test.js
+++ src/components/navigation-links-list/navigation-links-list.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import SocialNetworkList from './social-network-list';
+import NavigationLinksList from './navigation-links-list';
+describe('NavigationLinksList component', () => {
+    it('NavigationLinksList render', async () => {
+        render(<NavigationLinksList></NavigationLinksList>);
 
-describe('SocialNetworkList', () => {
-    it('SocialNetworkList render', async () => {
-        render(<SocialNetworkList></SocialNetworkList>);
         await waitFor(() => screen.getByRole('link'));
         expect(screen.getByRole('link')).toBeInTheDocument();
     });
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -12,5 +12,12 @@ export const handlers = [
                 }
             ])
         );
+    }),
+
+    rest.get('http://localhost:3000/navigation-link', (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json([{ id: 1, title: 'Products', link: '#' }])
+        );
     })
 ];
