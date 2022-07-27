eb75c424488057a69bd1380836385fd38d01112e
alekseikorotkov <korotkovaleksei@icloud.com>
2022-07-27 11:28:25 +0300

    feat: add features-list component

--- src/components/feature/feature.js
+++ src/components/feature/feature.js
@@ -1,3 +1,21 @@
 import './feature.css';
 
-const Feature = () => {};
+const Feature = (props) => {
+    const { title, description, imageName, alt } = props;
+
+    return (
+        <div className="feature">
+            <img
+                src={require(`../../assets/images/${imageName}`)}
+                alt={alt}
+                className="icon-feature"
+            />
+            <section>
+                <p className="title-feature">{title}</p>
+                <p className="description-feature">{description}</p>
+            </section>
+        </div>
+    );
+};
+
+export default Feature;
--- src/components/features-list/features-list.js
+++ src/components/features-list/features-list.js
@@ -1,5 +1,36 @@
 import './features-list.css';
-import { Feature } from '../feature/feature';
+import Feature from '../feature/feature';
+import { useState, useEffect } from 'react';
 
-const FeatureList = () => {};
+const FeatureList = () => {
+    const [featureList, setFeatureList] = useState([]);
+    const [isLoad, setIsLoad] = useState(false);
+    useEffect(() => {
+        const domain = process.env.REACT_APP_DOMAIN;
+        fetch(`${domain}/features`)
+            .then((response) => response.json())
+            .then((data) => {
+                setFeatureList(data);
+
+                setIsLoad(true);
+            });
+    }, []);
+    const showFeatures = featureList.map((item, index) => (
+        <Feature
+            key={index}
+            title={item.title}
+            description={item.description}
+            imageName={item.imageName}
+            atl={item.alt}
+        ></Feature>
+    ));
+
+    return (
+        <>
+            {isLoad && showFeatures.length >= 3 && (
+                <div className="feature-list"> {showFeatures} </div>
+            )}
+        </>
+    );
+};
 export default FeatureList;
