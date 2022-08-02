ec5a419fdc77e86012e00fb1bb0b3553e7f68f62
alekseikorotkov <korotkovaleksei@icloud.com>
2022-08-02 21:50:50 +0300

    feat: create modal component

--- src/components/feedback/feedback.js
+++ src/components/feedback/feedback.js
@@ -1,29 +1,60 @@
 import './feedback.css';
 import feedbackImg from '../../assets/images/feedback.jpeg';
 import Button from '../button/button';
+import Modal from '../modal/modal';
+import { useState } from 'react';
 const FeedBack = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openModal = () => {
+        setIsOpen(true);
+    };
+    const handleSubmit = () => {
+        console.log(' Submit send feedback to server');
+        setIsOpen(false);
+    };
+    const handleCancel = () => {
+        console.log('Cansel function!');
+        setIsOpen(false);
+    };
     return (
-        <div className="feed-back-bg">
-            <div className="container feed-back">
-                <div className="feedback-info">
-                    <p className="feedback-question">
-                        Have Any Questions? Call us 24/7
-                    </p>
-                    <p className="numberPhone">+1 (234) 567 89 00</p>
-                    <p className="feedback-title">FeedBack for Us</p>
-                    <p className="feedback-description">
-                        You could give complete the form after click the button
-                        below and give feedback for us
-                    </p>
-                    <Button title="Give FeedBack"></Button>
+        <>
+            <div className="feed-back-bg">
+                <div className="container feed-back">
+                    <div className="feedback-info">
+                        <p className="feedback-question">
+                            Have Any Questions? Call us 24/7
+                        </p>
+                        <p className="numberPhone">+1 (234) 567 89 00</p>
+                        <p className="feedback-title">FeedBack for Us</p>
+                        <p className="feedback-description">
+                            You could give complete the form after click the
+                            button below and give feedback for us
+                        </p>
+                        <button onClick={openModal}>Give FeedBack</button>
+                        <Button title="Give FeedBack"></Button>
+                        <Modal
+                            title="FeedBack"
+                            isOpen={isOpen}
+                            onSubmit={handleSubmit}
+                            onCancel={handleCancel}
+                        >
+                            <p>fasdlkghkjasdgjkasdg</p>
+                            <p>fasdlkghkjasdgjkasdg</p>
+
+                            <p>fasdlkghkjasdgjkasdg</p>
+
+                            <p>fasdlkghkjasdgjkasdg</p>
+                        </Modal>
+                    </div>
+                    <img
+                        className="feedback-img"
+                        src={feedbackImg}
+                        alt="feedback img"
+                    />
                 </div>
-                <img
-                    className="feedback-img"
-                    src={feedbackImg}
-                    alt="feedback img"
-                />
             </div>
-        </div>
+        </>
     );
 };
 export default FeedBack;
--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,15 +1,33 @@
-import { Portal } from 'react';
 import './modal.css';
-import Button from '../button/button';
-import { GrClose } from 'react-icons/fa';
+// import Button from '../button/button';
+import { GrClose } from 'react-icons/gr';
+import PropTypes from 'prop-types';
+import { useEffect } from 'react';
+const Modal = ({ title, isOpen, onCancel, onSubmit, children }) => {
+    const keydownHandler = ({ key }) => {
+        switch (key) {
+            case 'Escape':
+                onCancel();
+                break;
+            default:
+        }
+    };
 
-const Modal = (title, isOpen, onCancel, onSubmit, children) => {
+    useEffect(() => {
+        document.addEventListener('keydown', keydownHandler);
+        return () => document.removeEventListener('keydown', keydownHandler);
+    });
     return (
         <>
             {isOpen && (
-                <Portal>
-                    <div className="modalOverlay">
-                        <div className="modalWindow">
+                <div>
+                    <div onClick={onCancel} className="modalOverlay">
+                        <div
+                            className="modalWindow"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                            }}
+                        >
                             <div className="modalHeader">
                                 <div className="modalTitle">{title}</div>
                                 <div>
@@ -18,21 +36,30 @@ const Modal = (title, isOpen, onCancel, onSubmit, children) => {
                             </div>
                             <div className="modalBody">{children}</div>
                             <div className="modalFooter">
-                                <Button
-                                    title="Cancel"
-                                    onClick={onCancel}
-                                ></Button>
-                                <Button
-                                    title="Submit"
-                                    onSubmit={onSubmit}
-                                ></Button>
+                                <button onClick={onCancel}>Cancel</button>
+                                <button onClick={onSubmit}>Submit</button>
                             </div>
                         </div>
                     </div>
-                </Portal>
+                </div>
             )}
         </>
     );
+};
+Modal.propTypes = {
+    title: PropTypes.string,
+    isOpen: PropTypes.bool,
+    onCancel: PropTypes.func,
+    onSubmit: PropTypes.func,
+    children: PropTypes.node
+};
+
+Modal.defaultProps = {
+    title: 'Modal title',
+    isOpen: false,
+    onCancel: () => {},
+    onSubmit: () => {},
+    children: null
 };
 
 export default Modal;
